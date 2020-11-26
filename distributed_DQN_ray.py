e5a5b754d43cb2afc80b1cafa0d9a5f021c34c65
Dian Wang <wangdian1007@gmail.com>
2020-11-26 12:58:49 -0500

    remove old version

--- distributed_DQN_ray.py
+++ distributed_DQN_ray.py
@@ -3,223 +3,14 @@ import numpy as np
 import gym
 import tqdm
 import copy
+import time
 
 from agent.DQNAgent import DQNAgent
-from utils.ExperienceReplay import ReplayBuffer
-from utils.Schedule import LinearSchedule
 
-
-# @ray.remote
-class Actor(object):
-    def __init__(self, agent_configs, env_configs, param_server_remote, memory_server_remote):
-        # store the IDs
-        self.id = agent_configs['agent_id']
-
-        # store the agent model
-        self.agent = agent_configs['agent_model']
-
-        # init remote servers
-        self.remote_param_server = param_server_remote
-        self.remote_memory_server = memory_server_remote
-
-        # local experience replay buffer
-        self.local_buffer = []
-        self.buffer_size = env_configs['max_episode_time_steps']
-
-        # environment parameters
-        self.env = env_configs['env_fn']()
-        self.episode_time_steps = env_configs['max_episode_time_steps']
-
-        # running indicators
-        self.scheduled_eps = 1
-
-    def update_behavior_policy(self):
-        # synchronize the behavior policy with the latest parameters on the parameter server
-        self.agent.behavior_policy_net.load_state_dict(ray.get(self.remote_param_server.get_latest_model_params.remote()))
-        self.agent.behavior_policy_net.eval()
-        # synchronize the scheduled epsilon with the latest epsilon on the parameter server
-        self.scheduled_eps = ray.get(self.remote_param_server.get_scheduled_eps.remote())
-
-    def send_data(self):
-        # send the data to the memory server
-        self.remote_memory_server.add.remote(self.local_buffer)
-        # clear the local memory buffer
-        self.local_buffer = []
-
-    def run(self):
-        # synchronize the parameters
-        self.update_behavior_policy()
-        # initialize the environment
-        obs, rewards = self.env.reset(), []
-        # start data collection
-        while True:
-            # compute the epsilon
-            self.agent.eps = self.scheduled_eps
-            # get the action
-            action = self.agent.get_action(obs)
-            # interaction with the environment
-            next_obs, reward, done, _ = self.env.step(action)
-            # record rewards
-            rewards.append(reward)
-            # add the local buffer
-            self.local_buffer.append((obs, action, reward, next_obs, done))
-            # check termination
-            if done:
-                # G = 0
-                # for r in reversed(rewards):
-                #     G = r + 0.9995 * G
-                # print(f"Actor {self.id}: G = {G}, Eps = {self.scheduled_eps}")
-                # reset environment
-                obs, rewards = self.env.reset(), []
-                # synchronize the behavior policy
-                self.update_behavior_policy()
-                # send data to remote memory buffer
-                self.send_data()
-            else:
-                obs = next_obs
-
-
-# @ray.remote
-class MemoryServer(object):
-    def __init__(self, size):
-        self.size = size  # size of the memory
-        self.storage = ReplayBuffer(size)  # create a replay buffer
-
-    def get_size(self):
-        return len(self.storage)  # get the size of the replay buffer
-
-    def add(self, item_list):  # add transitions to the replay buffer
-        for item in item_list:
-            obs, act, reward, next_obs, d = item
-            self.storage.add(obs, act, reward, next_obs, d)
-
-    def sample(self, batch_size):  # sample a batch with size batch_size
-        return self.storage.sample_batch(batch_size)
-
-
-# @ray.remote
-class ParamServer(object):
-    def __init__(self, model_state_dict):
-        self.model_state_dict = model_state_dict  # parameters of the model
-        self.eps = 1  # epsilon for exploration
-
-    def sync_learner_model_params(self, model_stat_dict, eps):  # synchronize the parameters with the learner
-        self.model_state_dict = model_stat_dict
-        self.eps = eps
-
-    def get_latest_model_params(self):  # return the latest model parameters
-        return self.model_state_dict
-
-    def get_scheduled_eps(self):  # return the latest epsilon
-        return self.eps
-
-
-# @ray.remote
-class Learner(object):
-    def __init__(self, learn_params, env_params, param_server_remote, memory_server_remote):
-        # remote servers
-        self.remote_memory_server = memory_server_remote
-        self.remote_param_server = param_server_remote
-
-        # learning params
-        self.worker_num = learn_params['worker_num']
-        self.epochs = learn_params['epochs']  # training epochs
-        self.agent = learn_params['agent']  # model of the agent
-        self.batch_size = learn_params['batch_size']  # batch size
-        self.start_train_memory_size = learn_params['start_train_memory_size']  # minimal memory size
-
-        # schedule
-        self.scheduled_eps = 1
-        self.schedule = LinearSchedule(1, 0.01, self.epochs / 2)
-
-        # testing env
-        self.env_params = env_params
-        self.test_env = env_params['env_fn']()
-
-        # current training step
-        self.step = 0
-
-        # save results
-        self.returns = []
-
-    def sync_param_server(self):
-        self.remote_param_server.sync_learner_model_params.remote(self.agent.behavior_policy_net.state_dict(),
-                                                                  self.scheduled_eps)
-
-    def eval_policy(self, episode):
-        old_eps = self.agent.eps
-        returns = []
-        self.agent.eps = 0
-        for _ in range(episode):
-            G = 0
-            rewards = []
-            obs = self.test_env.reset()
-            for _ in range(self.env_params['max_episode_time_steps']):
-                action = self.agent.get_action(obs)
-                next_obs, reward, done, _ = self.test_env.step(action)
-                rewards.append(reward.item())
-                if done.item():
-                    for r in reversed(rewards):
-                        G = r + self.agent.gamma * G
-                    break
-                else:
-                    obs = next_obs
-            returns.append(G)
-
-        self.agent.eps = old_eps
-        return returns
-
-    def update(self):
-        self.step += 1
-        batch_data = ray.get(self.remote_memory_server.sample.remote(self.batch_size))
-        # compute the epsilon
-        self.scheduled_eps = self.schedule.get_value(self.step)
-        # update the behavior policy
-        loss = self.agent.update_behavior_policy(batch_data)
-        # send to the parameter server
-        self.sync_param_server()
-        return loss
-
-    def update_target(self):
-        self.agent.update_target_policy()
-
-    # def run(self):
-    #     # check if the memory server contains enough data
-    #     while ray.get(self.remote_memory_server.get_size.remote()) < self.start_train_memory_size:
-    #         continue
-    #
-    #     # start training
-    #     pbar = tqdm.trange(self.epochs)
-    #     for ep in pbar:
-    #         # sample a batch data
-    #         batch_data = ray.get(self.remote_memory_server.sample.remote(self.batch_size))
-    #
-    #         # update policy network
-    #         if not np.mod(ep, self.update_policy_freq):
-    #             # compute the epsilon
-    #             self.scheduled_eps = self.schedule.get_value(ep)
-    #             # update the behavior policy
-    #             self.agent.update_behavior_policy(batch_data)
-    #             # send to the parameter server
-    #             self.sync_param_server()
-    #
-    #         # update the target policy
-    #         if not np.mod(ep, self.update_target_freq):
-    #             # update the target network
-    #             self.agent.update_target_policy()
-    #
-    #         if not np.mod(ep, self.eval_policy_freq):
-    #             G = self.agent.eval_policy(env_params)
-    #             self.returns.append(G)
-    #             # print information
-    #             pbar.set_description(
-    #                 f'Epoch: {ep} |'
-    #                 f'G: {G:.2f} | '
-    #                 f'Eps: {self.scheduled_eps:.3f} | '
-    #                 f'Buffer: {ray.get(self.remote_memory_server.get_size.remote())}'
-    #             )
-    #
-    #     np.save(f'./w{self.worker_num}_returns.npy', self.returns)
+from distributed.actor import Actor
+from distributed.learner import Learner
+from distributed.memory_server import MemoryServer
+from distributed.param_server import ParamServer
 
 if __name__ == '__main__':
     ray.init()  # init the ray
@@ -251,7 +42,7 @@ if __name__ == '__main__':
     # initialize parameters for training
     train_params = {
         'agent': None,
-        'worker_num': 1,
+        'worker_num': 4,
         'memory_size': 50000,
         'batch_size': 128,
         'epochs': 50000,
@@ -267,18 +58,18 @@ if __name__ == '__main__':
     env_params['env_fn'] = env_fn
 
     # create the remote memory server
-    remote_memory_server = MemoryServer.remote(train_params['memory_size'])
+    remote_memory_server = ray.remote(MemoryServer).remote(train_params['memory_size'])
 
     # create the agent
     agent = DQNAgent(env_params, agent_params)
     agent_params['agent_model'] = copy.deepcopy(agent)
 
     # create the remote parameter server
-    remote_param_server = ParamServer.remote(agent.behavior_policy_net.state_dict())
+    remote_param_server = ray.remote(ParamServer).remote(agent.behavior_policy_net.state_dict())
 
     # create the remote learner server
     train_params['agent'] = copy.deepcopy(agent)
-    remote_learner = Learner.remote(train_params, remote_param_server, remote_memory_server)
+    remote_learner = ray.remote(Learner).remote(train_params, env_params, remote_param_server, remote_memory_server)
 
     # create the actors
     actor_num = train_params['worker_num']
@@ -286,11 +77,43 @@ if __name__ == '__main__':
     for i in range(actor_num):
         agent_params['agent_id'] = i
         agent_params['agent_model'] = DQNAgent(env_params, agent_params)
-        actors.append(Actor.remote(agent_params, env_params, remote_param_server, remote_memory_server))
+        actors.append(ray.remote(Actor).remote(agent_params, env_params, remote_param_server, remote_memory_server))
 
-    processes = [remote_learner]
+    processes = []
     for actor in actors:
         processes.append(actor)
 
     processes_running = [p.run.remote() for p in processes]
+    test_returns = []
+    while ray.get(remote_memory_server.get_size.remote()) < train_params['start_train_memory_size']:
+        continue
+
+    # start training
+    pbar = tqdm.trange(train_params['epochs'])
+    G = 0
+    t0 = time.time()
+    # testing_freq = 2*60
+    testing_freq = 10
+    for t in range(train_params['epochs']):
+        # sample a batch data
+        ray.get(remote_learner.update.remote())
+        ray.get(remote_learner.sync_param_server.remote())
+
+        # update the target policy
+        if not np.mod(t, train_params['update_target_freq']):
+            # update the target network
+            ray.get(remote_learner.update_target.remote())
+
+        if time.time() - t0 > (len(test_returns) + 1) * testing_freq:
+            G = np.average(ray.get(remote_learner.eval_policy.remote(10)))
+            test_returns.append(G)
+
+        # print information
+        pbar.set_description(
+            f'Step: {t} |'
+            f'G: {np.mean(test_returns[-5:]) if test_returns else 0:.2f} | '
+            # f'Buffer: {ray.get(self.remote_memory_server.get_size.remote())}'
+        )
+        pbar.update()
+    np.save("./parallel_returns.npy", test_returns)
     ray.wait(processes_running)
--- distributed_DQN_ray_block_stacking.py
+++ distributed_DQN_ray_block_stacking.py
@@ -10,110 +10,23 @@ from agent.block_stacking.dqn_wrapper import DQNBlock
 from network.models import ResUCatShared
 from env.block_stacking.env_wrapper import BlockStackingEnv
 from utils.ExperienceReplay import ReplayBuffer
+from distributed.actor import Actor
+from distributed.learner import Learner
+from distributed.memory_server import MemoryServer
+from distributed.param_server import ParamServer
 from utils.Schedule import LinearSchedule
 from utils.buffer import QLearningBuffer
 
 ExpertTransition = collections.namedtuple('ExpertTransition', 'state obs action reward next_state next_obs done step_left expert')
 
 
-def eval_policy(env, policy, episode, max_episode_time_steps, gamma=0.9):
-    # env_test = BlockStackingEnv(env_params['env_config'])
-    old_eps = policy.eps
-    returns = []
-    policy.eps = 0
-    for _ in range(episode):
-        G = 0
-        rewards = []
-        obs = env.reset()
-        for _ in range(max_episode_time_steps):
-            action = policy.get_action(obs)
-            next_obs, reward, done, _ = env.step(action)
-            rewards.append(reward.item())
-            if done.item():
-                for r in reversed(rewards):
-                    G = r + gamma * G
-                break
-            else:
-                obs = next_obs
-        returns.append(G)
-
-    policy.eps = old_eps
-    return np.average(returns)
-
-
 @ray.remote(num_gpus=0.25)
-class Actor(object):
-    def __init__(self, agent_params, env_params, remote_param_server, remote_memory_server):
-        # store the IDs
-        self.id = agent_params['agent_id']
-        self.agent = agent_params['agent_model']
-
-        # init remote servers
-        self.remote_param_server = remote_param_server
-        self.remote_memory_server = remote_memory_server
-
-        # local experience replay buffer
-        self.local_buffer = []
-        self.buffer_size = env_params['max_episode_time_steps']
-
-        # environment parameters
-        self.env = BlockStackingEnv(env_params['env_config'])
-        self.total_time_steps = env_params['total_time_steps']
-        self.episode_time_steps = env_params['max_episode_time_steps']
-
-        # running indicators
-        self.current_step = 0
-
-        self.rewards = []
-
-        self.schedule = LinearSchedule(1, 0.01, self.total_time_steps / 2)
-
-    def update_behavior_policy(self):
-        self.agent.behavior_policy_net.load_state_dict(ray.get(self.remote_param_server.get_model_params.remote()))
-        self.agent.behavior_policy_net.eval()
-        self.current_step = ray.get(self.remote_param_server.get_step_params.remote())
-
-    def send_data(self):
-        # send the data to the memory server
-        self.remote_memory_server.add.remote(self.local_buffer)
-        # clear the local memory buffer
-        self.local_buffer = []
-
-    def run(self):
-        # synchronize the parameters
-        self.update_behavior_policy()
-        # initialize the environment
-        obs, rewards = self.env.reset(), []
-        # start data collection
-        while True:
-            # compute the epsilon
-            self.agent.eps = self.scheduled_eps
-            # get the action
-            action = self.agent.get_action(obs)
-            # interaction with the environment
-            next_obs, reward, done, _ = self.env.step(action)
-            # record rewards
-            rewards.append(reward)
-            # add the local buffer
-            self.local_buffer.append((obs, action, reward, next_obs, done))
-            # check termination
-            if done:
-                # G = 0
-                # for r in reversed(rewards):
-                #     G = r + 0.9995 * G
-                # print(f"Actor {self.id}: G = {G}, Eps = {self.scheduled_eps}")
-                # reset environment
-                obs, rewards = self.env.reset(), []
-                # synchronize the behavior policy
-                self.update_behavior_policy()
-                # send data to remote memory buffer
-                self.send_data()
-            else:
-                obs = next_obs
+class BlockStackingActor(Actor):
+    pass
 
 
 @ray.remote
-class MemoryServer(object):
+class BlockStackingMemoryServer:
     def __init__(self, size):
         self.size = size
         self.storage = QLearningBuffer(size)
@@ -129,74 +42,37 @@ class MemoryServer(object):
 
 
 @ray.remote(num_gpus=0.25)
-class ParamServer(object):
-    def __init__(self, model_state_dict):
-        self.param_model_state_dict = model_state_dict
-        self.param_step = 0
-
-    def sync_params(self, model_stat_dict, param_step):
-        self.param_model_state_dict = model_stat_dict
-        self.param_step = param_step
-
-    def get_model_params(self):
-        return self.param_model_state_dict
-
-    def get_step_params(self):
-        return self.param_step
+class BlockStackingParamServer(ParamServer):
+    pass
 
 
 @ray.remote(num_gpus=0.25)
-class Learner(object):
-    def __init__(self, learn_params, env_params, remote_param_server, remote_memory_server):
-        # remote servers
-        self.remote_memory_server = remote_memory_server
-        self.remote_param_server = remote_param_server
+class BlockStackingLearner(Learner):
+    def eval_policy(self, episode):
+        old_eps = self.agent.eps
+        returns = []
+        self.agent.eps = 0
+        for _ in range(episode):
+            G = 0
+            rewards = []
+            obs = self.test_env.reset()
+            for _ in range(self.env_params['max_episode_time_steps']):
+                action = self.agent.get_action(obs)
+                next_obs, reward, done, _ = self.test_env.step(action)
+                rewards.append(reward.item())
+                if done.item():
+                    for r in reversed(rewards):
+                        G = r + self.agent.gamma * G
+                    break
+                else:
+                    obs = next_obs
+            returns.append(G)
 
-        # learning params
-        self.agent = learn_params['agent']
-        self.test_env = BlockStackingEnv(env_params['env_config'])
-
-        self.step = 0
-        self.batch_size = learn_params['batch_size']
-        self.start_train_step = learn_params['start_train_step']
-        self.total_time_steps = learn_params['total_time_steps']
-        self.update_target_freq = learn_params['update_target_freq']
-        self.update_policy_freq = learn_params['update_policy_freq']
-
-        # server synchronize frequency
-        self.sync_param_server_freq = learn_params['syn_param_freq']
-
-        self.returns = []
-        self.steps = []
-
-    def get_model_params(self):
-        return self.param_model
-
-    def get_step_params(self):
-        return self.param_step
-
-    def sync_param_server(self):
-        self.remote_param_server.sync_params.remote(self.agent.behavior_policy_net.state_dict(), self.step)
-
-    def set_step(self, step):
-        self.step = step
-
-    def update(self):
-        batch_data = ray.get(self.remote_memory_server.sample.remote(self.batch_size))
-        loss, td_error = self.agent.update_behavior_policy(batch_data)
-        self.step += 1
-        return loss
-
-    def update_target(self):
-        self.agent.update_target_policy()
-
-    def eval_policy(self):
-        return eval_policy(self.test_env, self.agent, 20, 10)
-
-
-ray.init(num_gpus=2)  # init the ray
+        self.agent.eps = old_eps
+        return returns
 
 if __name__ == '__main__':
+    ray.init(num_gpus=2)  # init the ray
     workspace = np.asarray([[0.35, 0.65],
                             [-0.15, 0.15],
                             [0, 0.50]])
@@ -207,14 +83,17 @@ if __name__ == '__main__':
     # init the params
     env_config = {'simulator': 'pybullet', 'env': 'block_stacking', 'workspace': workspace, 'max_steps': 10,
                   'obs_size': heightmap_size, 'fast_mode': True, 'action_sequence': 'xyp', 'render': False,
-                  'num_objects': 4, 'random_orientation': False, 'reward_type': 'sparse', 'simulate_grasp': True,
+                  'num_objects': 3, 'random_orientation': False, 'reward_type': 'sparse', 'simulate_grasp': True,
                   'robot': 'kuka', 'workspace_check': 'point', 'in_hand_mode': 'raw', 'heightmap_resolution': heightmap_resolution}
+
+    def env_fn():
+        return BlockStackingEnv(env_config)
 
     env_params = {
         'env_name': 'block_stacking',
         'max_episode_time_steps': 10,
         'total_time_steps': 10000,
-        'env_config': env_config
+        'env_fn': env_fn
     }
 
     # init the agent parameters
@@ -230,32 +109,33 @@ if __name__ == '__main__':
         'memory_size': 50000,
         'batch_size': 32,
         'episode_time_steps': 10,
-        'total_time_steps': 10000,
+        'epochs': 10000,
         'lr': 5e-5,
         'update_target_freq': 100,
         'update_policy_freq': 10,
-        'start_train_step': 100,
-        'syn_param_freq': 10
+        'start_train_memory_size': 100,
+        'syn_param_freq': 10,
+        'worker_num': 5
     }
 
     # create the remote memory server
-    remote_memory_server = MemoryServer.remote(train_params['memory_size'])
+    remote_memory_server = BlockStackingMemoryServer.remote(train_params['memory_size'])
     # create the agent
     agent = DQNBlock(workspace, heightmap_resolution, agent_params['device'], agent_params['lr'], agent_params['gamma'])
     train_params['agent'] = agent
     # agent_params['agent_model'] = agent
     # create the remote parameter server
-    remote_param_server = ParamServer.remote(agent.behavior_policy_net.state_dict())
+    remote_param_server = BlockStackingParamServer.remote(agent.behavior_policy_net.state_dict())
     # create the remote learner server
-    remote_learner = Learner.remote(train_params, env_params, remote_param_server, remote_memory_server)
+    remote_learner = BlockStackingLearner.remote(train_params, env_params, remote_param_server, remote_memory_server)
 
     # create the actors
-    actor_num = 5
+    # actor_num = 5
     actors = []
-    for i in range(actor_num):
+    for i in range(train_params['worker_num']):
         agent_params['agent_id'] = i
         agent_params['agent_model'] = DQNBlock(workspace, heightmap_resolution, agent_params['device'], agent_params['lr'], agent_params['gamma'])
-        actors.append(Actor.remote(agent_params, env_params, remote_param_server, remote_memory_server))
+        actors.append(BlockStackingActor.remote(agent_params, env_params, remote_param_server, remote_memory_server))
 
     processes = []
     for actor in actors:
@@ -265,15 +145,16 @@ if __name__ == '__main__':
 
     test_returns = []
     losses = []
-    while ray.get(remote_memory_server.get_size.remote()) < train_params['start_train_step']:
+    while ray.get(remote_memory_server.get_size.remote()) < train_params['start_train_memory_size']:
         continue
 
     # start training
-    pbar = tqdm.trange(train_params['total_time_steps'])
+    pbar = tqdm.trange(train_params['epochs'])
     G = 0
     t0 = time.time()
     testing_freq = 2*60
-    for t in range(train_params['total_time_steps']):
+    # testing_freq = 10
+    for t in range(train_params['epochs']):
         # sample a batch data
         loss = ray.get(remote_learner.update.remote())
         losses.append(loss)
@@ -285,8 +166,8 @@ if __name__ == '__main__':
             ray.get(remote_learner.update_target.remote())
 
         if time.time() - t0 > (len(test_returns) + 1) * testing_freq:
-        # if not np.mod(t, 200):
-            G = ray.get(remote_learner.eval_policy.remote())
+            # if not np.mod(t, 200):
+            G = np.mean(ray.get(remote_learner.eval_policy.remote(20)))
             test_returns.append(G)
 
         # print information
