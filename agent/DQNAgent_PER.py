6247a5b8608279157e21c29621dfba1014df43ff
XUChengguang <xu_chengguang@163.com>
2020-11-27 11:16:50 -0500

    chengguang-before-resolve-per

---
+++ agent/DQNAgent_PER.py
@@ -0,0 +1,179 @@
+import torch
+import gym
+import numpy as np
+from torch import nn
+from model.DeepNetworks import DeepQNet
+
+
+# customized weight initialization
+def customized_weights_init(m):
+    # compute the gain
+    gain = nn.init.calculate_gain('relu')
+    # init the convolutional layer
+    if isinstance(m, nn.Conv2d):
+        # init the params using uniform
+        nn.init.xavier_uniform_(m.weight, gain=gain)
+        nn.init.constant_(m.bias, 0)
+    # init the linear layer
+    if isinstance(m, nn.Linear):
+        # init the params using uniform
+        nn.init.xavier_uniform_(m.weight, gain=gain)
+        nn.init.constant_(m.bias, 0)
+
+
+class DQNAgent(object):
+    # initialize the agent
+    def __init__(self,
+                 env_params=None,
+                 agent_params=None,
+                 ):
+        # save the parameters
+        self.env_params = env_params
+        self.agent_params = agent_params
+
+        # environment parameters
+        self.action_space = np.linspace(0, env_params['act_num'], env_params['act_num'], endpoint=False).astype('uint8')
+        self.action_dim = env_params['act_num']
+        self.obs_dim = env_params['obs_dim']
+
+        # create behavior policy and target networks
+        self.dqn_mode = agent_params['dqn_mode']
+        self.use_obs = agent_params['use_obs']
+        self.gamma = agent_params['gamma']
+        self.behavior_policy_net = DeepQNet(self.obs_dim, self.action_dim)
+        self.target_policy_net = DeepQNet(self.obs_dim, self.action_dim)
+
+        # initialize target network with behavior network
+        self.behavior_policy_net.apply(customized_weights_init)
+        self.target_policy_net.load_state_dict(self.behavior_policy_net.state_dict())
+
+        # send the agent to a specific device: cpu or gpu
+        self.device = torch.device(agent_params['device'])
+        self.behavior_policy_net.to(self.device)
+        self.target_policy_net.to(self.device)
+
+        # optimizer
+        self.optimizer = torch.optim.Adam(self.behavior_policy_net.parameters(), lr=self.agent_params['lr'])
+
+        # other parameters
+        self.eps = 1
+
+    # get action
+    def get_action(self, obs):
+        if np.random.random() < self.eps:  # with probability eps, the agent selects a random action
+            action = np.random.choice(self.action_space, 1)[0]
+        else:  # with probability 1 - eps, the agent selects a greedy policy
+            obs = self._arr_to_tensor(obs).view(1, -1)
+            with torch.no_grad():
+                q_values = self.behavior_policy_net(obs)
+                action = q_values.max(dim=1)[1].item()
+        return action
+
+    # update behavior policy
+    def update_behavior_policy(self, batch_data):
+        # convert batch data to tensor and put them on device
+        batch_data_tensor = self._batch_to_tensor(batch_data)
+
+        # get the transition data
+        obs_tensor = batch_data_tensor['obs']
+        actions_tensor = batch_data_tensor['action']
+        next_obs_tensor = batch_data_tensor['next_obs']
+        rewards_tensor = batch_data_tensor['reward']
+        dones_tensor = batch_data_tensor['done']
+
+        # compute the q value estimation using the behavior network
+        pred_q_value = self.behavior_policy_net(obs_tensor)
+        pred_q_value = pred_q_value.gather(dim=1, index=actions_tensor)
+
+        # compute the TD target using the target network
+        if self.dqn_mode == 'vanilla':
+            # compute the TD target using vanilla method: TD = r + gamma * max a' Q(s', a')
+            # no gradient should be tracked
+            with torch.no_grad():
+                max_next_q_value = self.target_policy_net(next_obs_tensor).max(dim=1)[0].view(-1, 1)
+                td_target_value = rewards_tensor + self.agent_params['gamma'] * (1 - dones_tensor) * max_next_q_value
+        else:
+            # compute the TD target using double method: TD = r + gamma * Q(s', argmaxQ_b(s'))
+            with torch.no_grad():
+                max_next_actions = self.behavior_policy_net(next_obs_tensor).max(dim=1)[1].view(-1, 1).long()
+                max_next_q_value = self.target_policy_net(next_obs_tensor).gather(dim=1, index=max_next_actions).view(
+                    -1, 1)
+                td_target_value = rewards_tensor + self.agent_params['gamma'] * (1 - dones_tensor) * max_next_q_value
+                td_target_value = td_target_value.detach()
+
+        # compute the loss
+        td_loss = torch.nn.functional.mse_loss(pred_q_value, td_target_value)
+
+        # minimize the loss
+        self.optimizer.zero_grad()
+        td_loss.backward()
+        self.optimizer.step()
+
+    # update update target policy
+    def update_target_policy(self):
+        if self.agent_params['use_soft_update']:  # update the target network using polyak average (i.e., soft update)
+            # polyak ~ 0.95
+            for param, target_param in zip(self.behavior_policy_net.parameters(), self.target_policy_net.parameters()):
+                target_param.data.copy_(
+                    (1 - self.agent_params['polyak']) * param + self.agent_params['polyak'] * target_param)
+        else:  # hard update
+            self.target_policy_net.load_state_dict(self.behavior_policy_net.state_dict())
+
+    # load trained model
+    def load_model(self, model_file):
+        # load the trained model
+        self.behavior_policy_net.load_state_dict(torch.load(model_file, map_location=self.device))
+        self.behavior_policy_net.eval()
+
+    # auxiliary functions
+    def _arr_to_tensor(self, arr):
+        arr_tensor = torch.from_numpy(arr).float().to(self.device)
+        return arr_tensor
+
+    def _batch_to_tensor(self, batch_data):
+        # store the tensor
+        batch_data_tensor = {'obs': [], 'action': [], 'reward': [], 'next_obs': [], 'done': []}
+        # get the numpy arrays
+        obs_arr, action_arr, reward_arr, next_obs_arr, done_arr = batch_data
+        # convert to tensors
+        batch_data_tensor['obs'] = torch.tensor(obs_arr, dtype=torch.float32).to(self.device)
+        batch_data_tensor['action'] = torch.tensor(action_arr).long().view(-1, 1).to(self.device)
+        batch_data_tensor['reward'] = torch.tensor(reward_arr, dtype=torch.float32).view(-1, 1).to(self.device)
+        batch_data_tensor['next_obs'] = torch.tensor(next_obs_arr, dtype=torch.float32).to(self.device)
+        batch_data_tensor['done'] = torch.tensor(done_arr, dtype=torch.float32).view(-1, 1).to(self.device)
+
+        return batch_data_tensor
+
+    def eval_policy(self, env_params):
+        # create environment
+        env_test = gym.make(env_params['env_name'])
+        # set running statistics
+        old_eps = self.eps
+        run_num = env_params['run_eval_num']
+        returns = []
+
+        self.eps = 0
+        for r in range(run_num):
+            # reset domain
+            obs, rewards = env_test.reset(), []
+            # one rollout
+            for t in range(env_params['max_episode_time_steps']):
+                # get greedy policy
+                action = self.get_action(obs)
+                # interaction
+                next_obs, reward, done, _ = env_test.step(action)
+                # save rewards
+                rewards.append(reward)
+                # check termination
+                if done:
+                    G = 0
+                    for r in reversed(rewards):
+                        G = r + 0.9995 * G
+                    returns.append(G)
+                    break
+                else:
+                    obs = next_obs
+
+        # reset the epsilon
+        self.eps = old_eps
+        return np.mean(returns)
---
+++ distributed_per/actor_per.py
@@ -0,0 +1,77 @@
+import ray
+
+
+class Actor(object):
+    def __init__(self, agent_configs, env_configs, param_server_remote, memory_server_remote):
+        # store the IDs
+        self.id = agent_configs['agent_id']
+
+        # store the agent model
+        self.agent = agent_configs['agent_model']
+
+        # init remote servers
+        self.remote_param_server = param_server_remote
+        self.remote_memory_server = memory_server_remote
+
+        # local experience replay buffer
+        self.local_buffer = []
+        self.buffer_size = env_configs['max_episode_time_steps']
+
+        # environment parameters
+        self.env = env_configs['env_fn']()
+        self.episode_time_steps = env_configs['max_episode_time_steps'] - 1
+        self.total_train_steps = ray.get(self.remote_param_server.get_total_train_steps.remote())
+        self.current_train_step = 0
+
+        # running indicators
+        self.scheduled_eps = 1
+
+    def update_behavior_policy(self):
+        # synchronize the behavior policy with the latest parameters on the parameter server
+        self.agent.behavior_policy_net.load_state_dict(ray.get(self.remote_param_server.get_latest_model_params.remote()))
+        self.agent.behavior_policy_net.eval()
+        # synchronize the scheduled epsilon with the latest epsilon on the parameter server
+        self.scheduled_eps = ray.get(self.remote_param_server.get_scheduled_eps.remote())
+        # get the current train step
+        self.current_train_step = ray.get(self.remote_param_server.get_current_train_step.remote())
+
+    def send_data(self):
+        # send the data to the memory server
+        self.remote_memory_server.add.remote(self.local_buffer)
+        # clear the local memory buffer
+        self.local_buffer = []
+
+    def run(self):
+        # synchronize the parameters
+        self.update_behavior_policy()
+        # initialize the environment
+        obs, rewards = self.env.reset(), []
+        # start data collection until the training process terminates
+        while self.current_train_step < self.total_train_steps:
+            # compute the epsilon
+            self.agent.eps = self.scheduled_eps
+            # get the action
+            action = self.agent.get_action(obs)
+            # interaction with the environment
+            next_obs, reward, done, _ = self.env.step(action)
+            # record rewards
+            rewards.append(reward)
+            # add the local buffer
+            self.local_buffer.append((obs, action, reward, next_obs, done))
+            # check termination
+            if done:
+                # G = 0
+                # for r in reversed(rewards):
+                #     G = r + 0.9995 * G
+                # print(f"Actor {self.id}: G = {G}, Eps = {self.scheduled_eps}")
+                # reset environment
+                obs, rewards = self.env.reset(), []
+                # synchronize the behavior policy
+                self.update_behavior_policy()
+                # send data to remote memory buffer
+                self.send_data()
+            else:
+                obs = next_obs
+
+        print(f"Actor {self.id} terminates.")
+
